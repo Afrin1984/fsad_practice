59f54d6c15fa0069c64dd90b97085bc0d4c461c0
Afrin1984 <2400031984@kluniveristy.in>
2026-02-11 14:49:08 +0530

    11-02-2026

---
+++ SpringMVCDemoApp/src/main/java/com/klu/service/MessageService.java
@@ -0,0 +1,13 @@
+package com.klu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+	public String getMessage() {
+		return "Welcome to Spring Boot App";
+	}
+
+	
+
+}
